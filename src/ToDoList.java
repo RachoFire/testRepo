0a71d6d860f7ef6c289102a362c2e1aec09dab45
RachoFire <82930926+RachoFire@users.noreply.github.com>
2025-03-27 08:43:07 +1000

    Update ToDoList.java

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -85,45 +85,7 @@ public class ToDoList {
     /**
      * Requests the user for a new item description and adds it to the list.
      */
-    public void onAddItem() {
-        System.out.print("Enter the description of the item: ");
-        Scanner scanner = new Scanner(System.in);
-        String description = scanner.nextLine();
-        // TODO Now: Call the add(ToDoItem item) method of the user's to-do items list to add a new item
-    }
-
-    /**
-     * Requests the user for an item number and marks it as done.
-     */
-    public void onMarkAsDone() {
-        onShowAllItems();
-        System.out.print("Enter the number of the item to mark as done: ");
-        Scanner scanner = new Scanner(System.in);
-        int itemNumber = scanner.nextInt();
-        // TODO Now: Set the isDone field of the item at the specified index to true
-    }
-
-    /**
-     * Requests the user for an item number and marks it as undone.
-     */
-    public void onMarkAsUndone() {
-        onShowAllItems();
-        System.out.print("Enter the number of the item to mark as undone: ");
-        Scanner scanner = new Scanner(System.in);
-        int itemNumber = scanner.nextInt();
-        // TODO Now: Set the isDone field of the item at the specified index to false
-    }
-
-    /**
-     * Requests the user for an item number and deletes it from the list.
-     */
-    public void onDeleteItem() {
-        onShowAllItems();
-        System.out.print("Enter the number of the item to delete: ");
-        Scanner scanner = new Scanner(System.in);
-        int itemNumber = scanner.nextInt();
-        // TODO Now: Remove the item at the specified index from the user's to-do items list
-    }
+    git push --set-upstream origin test
 
     /**
      * Exits the application, which sets the `isRunning` flag to false.
