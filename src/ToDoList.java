d0cf4dda8789f5f10c2183a460758c7b01b5a120
RachoFire <82930926+RachoFire@users.noreply.github.com>
2025-03-27 08:44:10 +1000

    Update ToDoList.java

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -82,11 +82,66 @@ public class ToDoList {
         }
     }
 
-    /**
-     * Requests the user for a new item description and adds it to the list.
-     */
-    git push --set-upstream origin test
+    public void onAddItem() {
+        System.out.print("Enter the description of the item: ");
+        Scanner scanner = new Scanner(System.in);
+        String description = scanner.nextLine();
+        // TODO Now: Call the add(ToDoItem item) method of the user's to-do items list to add a new item
+        user.getToDoItems().add(new ToDoItem(description));
+    }
 
+    /**
+     * Requests the user for an item number and marks it as done.
+     */
+    public void onMarkAsDone() {
+        onShowAllItems();
+        System.out.print("Enter the number of the item to mark as done: ");
+        Scanner scanner = new Scanner(System.in);
+        int itemNumber = scanner.nextInt();
+        // TODO Now: Set the isDone field of the item at the specified index to true
+        Iterator i = user.getToDoItems().iterator();
+        while (i.hasNext()) {
+            int loopCount = 0;
+            if (itemNumber == loopCount) {
+                ToDoItem toDo = (ToDoItem) i.next();
+                toDo.setIsDone(true);
+            }
+            loopCount++;
+        }
+
+    }
+
+    /**
+     * Requests the user for an item number and marks it as undone.
+     */
+    public void onMarkAsUndone() {
+        onShowAllItems();
+        System.out.print("Enter the number of the item to mark as undone: ");
+        Scanner scanner = new Scanner(System.in);
+        int itemNumber = scanner.nextInt();
+        // TODO Now: Set the isDone field of the item at the specified index to false
+        Iterator i = user.getToDoItems().iterator();
+        while (i.hasNext()) {
+            int loopCount = 0;
+            if (itemNumber == loopCount) {
+                ToDoItem toDo = (ToDoItem) i.next();
+                toDo.setIsDone(false);
+            }
+            loopCount++;
+        }
+    }
+
+    /**
+     * Requests the user for an item number and deletes it from the list.
+     */
+    public void onDeleteItem() {
+        onShowAllItems();
+        System.out.print("Enter the number of the item to delete: ");
+        Scanner scanner = new Scanner(System.in);
+        int itemNumber = scanner.nextInt();
+        // TODO Now: Remove the item at the specified index from the user's to-do items list
+        user.getToDoItems().remove(itemNumber);
+    }
     /**
      * Exits the application, which sets the `isRunning` flag to false.
      */
